ab00ccea97a46f9bff20ac39e28cc30562352571
coderedalfa420-byte <coderedalfa420@gmail.com>
2023-01-01 12:00:00 +0530

    Clear repository

--- Backend/routes/weather.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getWeather } = require('../controller/weatherController');
-const router = express.Router();
-
-router.get('/:city', getWeather);
-
-module.exports = router;
